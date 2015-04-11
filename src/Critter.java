3f2f394645219925b8e49fe42aa79a0eeb4c8cdd
ahtml <anandhtml@gmail.com>
2015-04-11 10:05:48 -0400

    Added Critter abstract class with variables and getters and setters for each.

---
+++ src/Critter.java
@@ -0,0 +1,36 @@
+
+public abstract class Critter {
+	
+	private int health;
+	private int speed;
+	private int position;
+	private int id;
+	
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
